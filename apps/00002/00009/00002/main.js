187929e51330603b287e393e7f5d8cd47499c29a
mozhaonan <2885354314@qq.com>
2022-01-05 18:11:24 +0800

    一般提交

---
+++ apps/00002/00009/00002/main.js
@@ -0,0 +1,32 @@
+const { BrowserWindow, app, ipcMain } = require('electron')
+const path = require('path');
+
+app.whenReady().then(async () => {
+    const mainWindow = new BrowserWindow({
+        webPreferences: { nodeIntegration: true },
+        // 定义预加载文件
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    mainWindow.loadFile('index.html')
+
+    ipcMain.on('give-me-a-stream', async (event, msg) => {
+        // The renderer has sent us a MessagePort that it wants us to send our
+        // response over.
+        const [replyPort] = event.ports
+    
+        // Here we send the messages synchronously, but we could just as easily store
+        // the port somewhere and send messages asynchronously.
+        
+        for ( let i = 0; i < msg.count; i++) {
+            replyPort.postMessage(msg.element)
+        }
+    
+        // We close the port when we're done to indicate to the other end that we
+        // won't be sending any more messages. This isn't strictly necessary--if we
+        // didn't explicitly close the port, it would eventually be garbage
+        // collected, which would also trigger the 'close' event in the renderer.
+        replyPort.close()
+    });
+})
